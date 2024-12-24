aa1ca668b2163846cec587946d8d8b85c1d1aad8
Vsevolod <sevabakutov322@gmail.com>
2024-12-24 04:24:54 +0100

    added third oauth2 endpoint

--- module/move/gspread/tests/mock/header_tests.rs
+++ module/move/gspread/tests/mock/header_tests.rs
@@ -1,5 +1,6 @@
 use httpmock::prelude::*;
 use reqwest;
+use serde_json::json;
 
 #[ tokio::test ]
 async fn oauth2_first_endpoint_with_mock()
@@ -46,7 +47,7 @@ async fn oauth2_second_endpoint_with_mock()
   let mock = server.mock( | when, then | {
     when.path( "/o/oauth2/auth" )
       .query_param( "scope", "https://..." );
-    then.status( 200 );
+    then.status( 302 );
   } );
 
   // in real program at that point we have to open generated url and give access to our program from browser
@@ -54,27 +55,45 @@ async fn oauth2_second_endpoint_with_mock()
 
   mock.assert();
 
-  assert_eq!( response.status(), 200 );
+  assert_eq!( response.status(), 302 );
 }
 
-// #[ tokio::test ]
-// async fn get_header_with_mock_test()
-// {
-//   let server = MockServer::start();
-//   let mock = server.mock( | when, then | {
-//     when.method( GET )
-//       .path( "/spreadsheets/d/1234" )
-//       .query_param( "tab1!A1:Z1" )
-//     then.status( 200 )
-//       .header( "content-type", "application/json" )
-//       .body( r#"["Name", "Surname", "Age"]"# );
-//   });
+#[ tokio::test ]
+async fn oauth2_third_endpoint_with_mock()
+{
+  let server = MockServer::start();
+  let mock = server.mock( | when, then | {
+    when.method( POST )
+      .path( "/token" )
+      .header("Content-Type", "application/json" )
+      .body( r#"code=AUTHORIZATION_CODE&client_secret=YOUR_CLIENT_SECRET&"# );
+    then.status( 200 )
+      .header("Content-Type", "application/json" )
+      .json_body
+      ( 
+        json!
+        ( 
+          {
+            "access_token" : "access_token",
+            "token_type" : "Bearer",
+            "expires_in" : "3600",
+            "scope" : "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email openid"
+          }
+        ) 
+      );
+  });
 
-//   let response = reqwest::get( server.url( "/spreadsheets/d/1234?tab1!A1:Z1" ) )
-//   .await
-//   .unwrap();
+  let body = r#"code=AUTHORIZATION_CODE&client_secret=YOUR_CLIENT_SECRET&"#;
 
-//   mock.assert();
+  let response = reqwest::Client::new()
+  .post( server.url( "/token" ) )
+  .header( "Content-Type", "application/json" )
+  .body( body )
+  .send()
+  .await
+  .unwrap();
 
-//   assert_eq!( response.status(), 200 )
-// }
+  mock.assert();
+
+  assert_eq!( response.status(), 200 );
+}
