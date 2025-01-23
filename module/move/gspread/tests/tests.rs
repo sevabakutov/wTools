62bfa94823b4edded522c89650f4c0d0429ae038
Vsevolod <sevabakutov322@gmail.com>
2025-01-23 12:28:04 +0100

    uncomment with_online

--- module/move/gspread/tests/tests.rs
+++ module/move/gspread/tests/tests.rs
@@ -3,9 +3,8 @@ use gspread as the_module;
 #[ allow( unused_imports ) ]
 use test_tools::exposed::*;
 
-// Uncomment it in case you want to test that feature.
-// #[ cfg( feature = "with_online" ) ]
-// mod inc;
+#[ cfg( feature = "with_online" ) ]
+mod inc;
 
 #[ cfg( feature = "default" ) ]
 mod mock;
